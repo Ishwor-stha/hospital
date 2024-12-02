9b5cc9622174ab49170b233e743734f4df71d728
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-12-02 16:03:44 +0545

    added comment

--- controller/doctorController.js
+++ controller/doctorController.js
@@ -44,7 +44,7 @@ module.exports.getDoctorByPhoneOrName = async (req, res, next) => {
             //check if there is query presented on URL
             if (Object.keys(req.query).length <= 0) return next(new errorHandling("No query is given", 404));
             let searching = {};//create empty object variable
-//if there is name on qurey
+            //if there is name on qurey
             if (req.query.name) searching["name"] = { $regex: req.query.name, $options: "i" };//case inscensitive and finds name similar to given input
             // if there is phone in qurey
             if (req.query.phone) searching["phone"] = req.query.phone;
@@ -80,7 +80,7 @@ module.exports.createDoctor = async (req, res, next) => {
             // if body is empty
             if (Object.keys(req.body).length <= 0) return next(new errorHandling("The body is empty", 404))
             //list of fields
-                let list = ["name", "department", "specialization", "experience", "phone", "email", "availability", "password", "confirmPassword"];
+            let list = ["name", "department", "specialization", "experience", "phone", "email", "availability", "password", "confirmPassword"];
             let upload = {}
             //filter only fields which are presented on list array
             for (key in req.body) {
@@ -89,7 +89,7 @@ module.exports.createDoctor = async (req, res, next) => {
                     upload[key] = req.body[key];
                 }
             }
-//create data on database
+            //create data on database
             const create = await doctorModel.create(upload);
             //data creation failed
             if (!create) return next(new errorHandling("Cannot create doctor please try again", 400));
@@ -100,7 +100,7 @@ module.exports.createDoctor = async (req, res, next) => {
 
             });
         } else {
-// if user is not root or admin
+            // if user is not root or admin
             return next(new errorHandling("You donot have enough permission to perform this task", 404));
         }
 
@@ -108,7 +108,7 @@ module.exports.createDoctor = async (req, res, next) => {
 
 
     } catch (error) {
-// this erorr code is for duplicate email
+        // this erorr code is for duplicate email
         if (error.code === 11000) return next(new errorHandling("Email already exists", 404))
         return next(new errorHandling(error.message, error.statusCode || 500));
     }
@@ -119,12 +119,12 @@ module.exports.createDoctor = async (req, res, next) => {
 
 module.exports.modifyDoctor = async (req, res, next) => {
     try {
-//allow only if user is admin or root
+        //allow only if user is admin or root
         if ((req.admin.role === "root" || req.admin.role === "admin")) {
             // if body is empty
             if (Object.keys(req.body).length <= 0) return next(new errorHandling("The body is empty", 404))
             // listing the possible keys of object
-                let list = ["name", "department", "specialization", "experience", "phone", "email", "availability", "password", "confirmPassword"];
+            let list = ["name", "department", "specialization", "experience", "phone", "email", "availability", "password", "confirmPassword"];
             let upload = {}
             // if req.body contain email
             if (req.body.email) {
@@ -149,7 +149,7 @@ module.exports.modifyDoctor = async (req, res, next) => {
             const id = req.params.id
             // if no id is given on URL
             if (!id) return next(new errorHandling("Please provide id of a doctor", 400))
-                //iterate every key on req.body
+            //iterate every key on req.body
             for (key in req.body) {
                 // if key matched with the above list array
                 if (list.includes(key)) {
@@ -175,7 +175,7 @@ module.exports.modifyDoctor = async (req, res, next) => {
             // update fails
             if (!update || Object.keys(update).length <= 0) return next(new errorHandling("Cannot update details", 404))
             // send response
-                res.status(200).json({
+            res.status(200).json({
                 status: true,
                 message: `${Object.keys(upload).filter(key => key !== "confirmPassword").join(", ")} updated successfully`
 
@@ -211,7 +211,7 @@ module.exports.deleteDoctor = async (req, res, next) => {
             // if no doctor by this id
             if (!delDoctor || Object.keys(delDoctor).length <= 0) return next(new errorHandling("No doctor found by this id", 404))
             // send response
-                res.status(200).json({
+            res.status(200).json({
                 status: true,
                 message: `${delDoctor.name} deleted sucessfully`
             })
@@ -237,28 +237,28 @@ module.exports.doctorLogin = async (req, res, next) => {
         if (keys.length !== 2 || !keys.includes('email') || !keys.includes('password')) {
             return next(new errorHandling("Request body must only contain 'email' and 'password'", 400))
         }
-// destructuring email and password from req.body
+        // destructuring email and password from req.body
         const { email, password } = req.body
         // validate email
         if (!emailValidation(email)) return next(new errorHandling("Please enter valid email address", 400))
-// check email on database
+        // check email on database
         const doctor = await doctorModel.findOne({ email })
-// if no detais found by the email
+        // if no detais found by the email
 
         if (!doctor || Object.keys(doctor).length <= 0) return next(new errorHandling("No doctor found by this email", 404))
-            // store the password from database 
+        // store the password from database 
 
         const dbPassword = doctor.password
         // compare the user password and database password
         const isvalid = await bcrypt.compare(password, dbPassword)
-// if password is not valid
+        // if password is not valid
         if (!isvalid) return next(new errorHandling("Password is incorrect", 400))
-            // create payload for jwt
+        // create payload for jwt
         const payload = {
             adminId: doctor._id,
             role: doctor.role,
         };
-// create jwt token
+        // create jwt token
         const token = jwt.sign(payload, process.env.jwtSecretKey, { expiresIn: '1h' });
         // send jwt cooke 
         res.cookie("auth_token", token, {
@@ -289,15 +289,15 @@ module.exports.updateDoctor = async (req, res, next) => {
         // allow if the user is doctor
         if (req.admin.role != "doctor") return next(new errorHandling("Only a doctor can perform this task", 404));
         // if the req.body is empty
-        if(Object.keys(req.body).length<=0)return next(new errorHandling("Empty body",404));
+        if (Object.keys(req.body).length <= 0) return next(new errorHandling("Empty body", 404));
         // id from jwtCheck middleware (./admiAuthController)
         const id = req.admin.adminId;
         // if no password and confirmPassword on req.body
         if (!req.body.password || !req.body.confirmPassword) return next(new errorHandling("Password or confirmPassword is empty", 400));
         // check the password and confirmPassword
-        if(req.body.password!==req.body.confirmPassword)return next(new errorHandling("Passoword and confirm password doesnot match",400));
+        if (req.body.password !== req.body.confirmPassword) return next(new errorHandling("Passoword and confirm password doesnot match", 400));
         // check the length of password
-        if(req.body.password.length<8) return next(new errorHandling("Password length must be atleast 8 character"),400);
+        if (req.body.password.length < 8) return next(new errorHandling("Password length must be atleast 8 character"), 400);
 
         const modify = {};
         // hash password
@@ -305,10 +305,10 @@ module.exports.updateDoctor = async (req, res, next) => {
         // store hashed password
         modify["password"] = hashPassword;
         // change the value of confirmPassword to undefined
-        modify["confirmPassword"]=undefined;
-// update the password of the doctor
+        modify["confirmPassword"] = undefined;
+        // update the password of the doctor
 
-        const update = await doctorModel.findByIdAndUpdate(id, modify,{
+        const update = await doctorModel.findByIdAndUpdate(id, modify, {
             new: true, // Return the updated document
             runValidators: true // Run schema validators
         });
