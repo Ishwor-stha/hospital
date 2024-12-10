9c04ec2e9af63b7a1e12e75a458fe7ba66ee364a
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-12-10 13:18:38 +0545

    added viewSpecific MedicalReport

--- controller/medicalController.js
+++ controller/medicalController.js
@@ -7,11 +7,11 @@ module.exports.checkingPatientAndData = async (req, res, next) => {
     try {
         //if user is not doctor throw error
         if (req.admin.role !== "doctor") {
-            return next(new errorHandling("You don't have enough permission to create a medical report", 403));
+            return next(new errorHandling("This task is restricted for authorized users only.", 403));
         }
         //if the keys on req.body object is zero then throw error 
         if (Object.keys(req.body).length === 0) {
-            return next(new errorHandling("Empty body", 400));
+            return next(new errorHandling("Empty request body: Ensure you're sending the correct information.", 400));
         }
         // if the req.originalUrl path matches 
         if (req.originalUrl.split("?")[0] === "/api/doctor/create-report") {
@@ -19,13 +19,13 @@ module.exports.checkingPatientAndData = async (req, res, next) => {
             const { patientId } = req.query;
             // if there is no patient id then throw error
             if (!patientId) {
-                return next(new errorHandling("No PatientId is given in query", 400));
+                return next(new errorHandling("Empty patient id on query: Ensure you're sending the correct information.", 400));
             }
             // search the patient by id
             const patient = await patientModel.findById(patientId, "name");
             // if no patient found then throw error
             if (!patient) {
-                return next(new errorHandling("No Patient found by this ID", 404));
+                return next(new errorHandling("No patient record found for the provided ID. Please check and try again.", 404));
             }
             // store the patient object at req.patient object for the use of next controller
             req.patient = patient;
@@ -36,13 +36,13 @@ module.exports.checkingPatientAndData = async (req, res, next) => {
             const { medicalId } = req.query;
             // if there is no medical id on the query then throw error
             if (!medicalId) {
-                return next(new errorHandling("No medicalId is given in query", 400));
+                return next(new errorHandling("Empty medical id on query: Ensure you're sending the correct information.", 400));
             }
             // searching the medical report from id passes through the query
             const medicalRecord = await medicalModel.findById(medicalId, "patient_id");
             // if there is no medical report in the database then throw error
             if (!medicalRecord) {
-                return next(new errorHandling("No medical report found by this ID", 404));
+                return next(new errorHandling("No medical record found for the provided ID. Please check and try again.", 404));
             }
         }
         // go to the next controller
@@ -77,12 +77,12 @@ module.exports.createReport = async (req, res, next) => {
         const save = await medicalModel.create(upload);
         // failed to create medical report
         if (!save) {
-            return next(new errorHandling("Cannot create medical record", 500));
+            return next(new errorHandling("Creation of medical report was not successful. Please retry.", 500));
         }
         // send sucess message
         res.status(200).json({
             status: true,
-            message: `${req.patient.name}'s medical report created successfully`
+            message: `${req.patient.name}'s medical report created successfully,`
         });
     } catch (error) {
         return next(new errorHandling(error.message, error.statusCode || 500));
@@ -110,12 +110,12 @@ module.exports.updateReport = async (req, res, next) => {
         const save = await medicalModel.findByIdAndUpdate(req.query.medicalId, upload, { new: true });
         // if falis to update then throw error
         if (!save) {
-            return next(new errorHandling("Cannot update medical record", 500));
+            return next(new errorHandling("Cannot update medical record. Please retry.", 500));
         }
         // send sucess response
         res.status(200).json({
             status: true,
-            message: "Medical report updated successfully"
+            message: "Medical report updated successfully."
         });
     } catch (error) {
         return next(new errorHandling(error.message, error.statusCode || 500));
@@ -134,15 +134,15 @@ module.exports.deleteMedicalReport = async (req, res, next) => {
         // destructruring the medicalId from req.body object
         const { medicalId } = req.query;
         // if there is no medicalId in the req.query object then throw error 
-        if (!medicalId) return next(new errorHandling("No medical id is given", 400));
+        if (!medicalId) return next(new errorHandling("Empty medical id query: Ensure you're sending the correct information.", 400));
         // delete the medical record
         const del = await medicalModel.findByIdAndDelete(medicalId);
         // if the deletiion fails then throw error 
-        if (!del) return next(new errorHandling("Cannot delete medical report"), 500);
+        if (!del) return next(new errorHandling("Medical Report was not deleted. Please retry."), 500);
         // send sucess resposne
         res.status(200).json({
             status: true,
-            message: "Medical report deleted sucessfully "
+            message: "Medical report deleted sucessfully."
         });
 
 
@@ -150,11 +150,15 @@ module.exports.deleteMedicalReport = async (req, res, next) => {
         return next(new errorHandling(error.message, error.statusCode || 500));
     }
 }
+
+//@method:DELETE
+// @endpoint:localhost:3000:/api/admin/view-medical-report
+// @desc:controller to view  medical report by root or admin
 module.exports.viewMedicalReport = async (req, res, next) => {
     try {
-        if (!["root", "admin"].includes(req.admin.role)) return next(new errorHandling("You donot have enough permission to perform this task", 403));
+        if (!["root", "admin"].includes(req.admin.role)) return next(new errorHandling("This task is restricted for authorized users only.", 403));
         const view = await medicalModel.find();
-        if (!view || Object.keys(view).length <= 0) return next(new errorHandling("No medical report found on database", 404));
+        if (!view || Object.keys(view).length <= 0) return next(new errorHandling("No medical report record found in the database.", 404));
         res.status(200).json({
             status: true,
             medicalReport: view
@@ -164,16 +168,22 @@ module.exports.viewMedicalReport = async (req, res, next) => {
     }
 }
 
+//@method:DELETE
+// @endpoint:localhost:3000:/api/doctor/view-report
+// @desc:controller to delete  medical report by doctor or patient
 module.exports.viewSpecificMedicalReport = async (req, res, next) => {
     try {
-        if (req.admin.role !== "doctor") return next(new errorHandling("You donot have enough permission to perform this task", 403));
-        const doctorId = req.admin.adminId;
-        const view = await medicalModel.find({ doctor_id: doctorId });
-        if (!view || Object.keys(view).length <= 0) return next(new errorHandling("No medical report is created ", 404));
+        if (!["doctor","patient"].includes(req.admin.role)) return next(new errorHandling("This task is restricted for authorized users only.", 403));
+        const ID = req.admin.adminId;
+        if(!ID || Object.keys(ID).length<=0)return next(new errorHandling("Empty id of user: Ensure you're logged in.",400));
+        let view; 
+        if(req.admin.role==="doctor")view= await medicalModel.find({ doctor_id: ID });
+        if(req.admin.role==="patient")view= await medicalModel.find({ patient_id: ID });
+        if (!view || Object.keys(view).length <= 0) return next(new errorHandling("No medical report record found in the database.", 404));
         res.status(200).json({
             status: true,
             medicalReport: view
-        })
+        });
     } catch (error) {
         return next(new errorHandling(error.message, error.statusCode || 500));
     }
--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -3,6 +3,7 @@ const { checkJwt } = require("../controller/adminAuthController");
 // const Router=express.Router()
 const { getAllPatient, postPatient, getPatientByPatientId, updatePatient, deletePatient, getPatientByName, patientLogin, forgetPassword, resetPassword } = require("../controller/patientController");
 const { createAppointment, viewPatientAppointment } = require("../controller/appointmentControlelr");
+const{viewSpecificMedicalReport}=require("../controller/medicalController");
 
 
 
@@ -25,6 +26,8 @@ Router.route("/create-appointment").post(checkJwt, createAppointment);
 Router.route("/view-appointment").get(checkJwt, viewPatientAppointment);
 
 Router.route("/delete-patient/:id").delete(checkJwt, deletePatient);
+
+Router.route("/view-report").get(checkJwt,viewSpecificMedicalReport);
 
 Router.route("/search").get(checkJwt, getPatientByName);
 
