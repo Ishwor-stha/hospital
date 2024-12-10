5e2463ca34d925fd85c0bc69196134a57d803709
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-12-10 12:39:04 +0545

    s

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,9 @@
-module.exports=(err,req,res,next)=>{
+module.exports = (err, req, res, next) => {
     // all the error details comes from the user through ../utils/error handling
-    res.status(err.statusCode ||500).json({
-        status:err.status,
-        message:err.message,
-        detail:process.env.NODE_ENV==="development"?err.stack:" "
+    res.status(err.statusCode || 500).json({
+        status: err.status,
+        message: err.message,
+        detail: process.env.NODE_ENV === "development" ? err.stack : " "
 
     });
 }
